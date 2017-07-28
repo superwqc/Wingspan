66f1d4ff37b372da9e33b78b092674d28645f80d
superwqc <superwqc@163.com>
2017-07-28 14:12:31 +0800

    RISConfig

---
+++ PACS4.0/RISConfig/RISConfigItemNames.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Restore.Utilities;
+
+namespace Restore.FIIS.BC.Configs.Common
+{
+    internal class RISConfigItemNames
+    {
+        public const string OperateUserCode = "OperateUserCode";
+        public const string UserList = "UserList";
+
+        public const string IsSimpleMode = "IsSimpleMode";
+
+
+        public const string ExamLockValidTimeSpan = "ExamLockValidTimeSpan";
+        public const string FromPrintedToRejectedValidTimeSpan = "FromPrintedToRejectedValidTimeSpan";
+
+        public const string OnDutyTimeBegin = "OnDutyTimeBegin";
+        public const string OnDutyTimeEnd = "OnDutyTimeEnd";
+        public const string OnDutyReviewUserCode = "OnDutyReviewUserCode";
+
+        public const string PACSGatewayImplement = "PACSGatewayImplement";
+        public const string PACSGatewayImplementList = "PACSGatewayImplementList";
+
+        public const string HISGatewayImplement = "HISGatewayImplement";
+        public const string HISGatewayImplementList = "HISGatewayImplementList";
+
+        public const string PatientIDGeneratorImplement = "PatientIDGeneratorImplement";
+        public const string PatientIDGeneratorImplementList = "PatientIDGeneratorImplementList";
+
+        public const string AccessionNOGeneratorImplement = "AccessionNOGeneratorImplement";
+        public const string AccessionNOGeneratorImplementList = "AccessionNOGeneratorImplementList";
+
+        public const string DataPrinterImplement = "DataPrinterImplement";
+        public const string DataPrinterImplementList = "DataPrinterImplementList";
+
+        public const string SaveRISConfig = "SaveRISConfig";
+
+        public const string AETitleByWorkStation = "AETitleByWorkStation";
+        public const string AETitleListByWorkStation = "AETitleListByWorkStation";
+
+        public const string SaveDcmConfig = "SaveDcmConfig";
+
+        public const string PACSGatewayUrl = "PACSGatewayUrl";
+    }
+}
