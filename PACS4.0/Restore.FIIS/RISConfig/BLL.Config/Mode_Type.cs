f19ce41bd5f297f13e2733d070ee14ebdce741fd
superwqc <superwqc@163.com>
2017-08-01 17:07:41 +0800

    Restore.FIIS

---
+++ PACS4.0/Restore.FIIS/RISConfig/BLL.Config/Mode_Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restore.FIIS.BLL
+{
+    public class Mode_Type
+    {
+
+        public  const string BASE_KEY = "Simple";
+        public  const string DETECTING_NAME = "SimpleMode";
+
+    }
+}
